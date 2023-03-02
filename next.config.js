6cf16e9018f1f72a5e9ddcadc5cdf756f4cc97da
Lukas Mann <lukas@lvmann.dk>
2023-03-02 10:45:40 +0100

    Allowed "exclude" and "limit" headers

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ module.exports = {
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
 					{ key: "Access-Control-Allow-Origin", value: "*" },
 					{ key: "Access-Control-Allow-Methods", value: "GET" },
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "exclude,limit",
+					},
 				],
 			},
 		];
